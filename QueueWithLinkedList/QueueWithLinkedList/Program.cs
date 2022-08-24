a250547b279f36e7cd25f2d6891654724fcbeac4
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 12:07:38 +0430

    Initial Repo

---
+++ QueueWithLinkedList/QueueWithLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using QueueWithLinkedList;
+
+var queue = new Queue();
+
+queue.Dequeue();
+queue.Enqueue(1);
+queue.Enqueue(4);
+queue.Dequeue();
+queue.Enqueue(5);
+queue.Enqueue(6);
+
+//queue.Dequeue();
+//queue.Dequeue();
+//queue.Dequeue();
+
+Console.WriteLine("-----------------------------------");
+
+queue.Print();
---
+++ QueueWithLinkedList/QueueWithLinkedList/Queue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueWithLinkedList
+{
+    public class Queue
+    {
+        public Node head;
+
+        public void Enqueue(int value)
+        {
+            if (head == null)
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+                head = newItem;
+            }
+            else
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+
+                Node node = head;
+                while(node.next != null)
+                {
+                    node = node.next;
+                }
+
+                node.next = newItem;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                Console.WriteLine(head.value);
+                Node temp = head.next;
+                head = null;
+                head = temp;
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+
+            }
+            else
+            {
+                Node node = head;
+                while (node != null)
+                {
+                    Console.WriteLine(node.value);
+                    node = node.next;
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
